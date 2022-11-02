eb48c84d8fdd2df1f82b126a6f57789ce1720e91
jonatan herran <jonatan.herran@phidias.com.co>
2022-11-02 09:10:46 -0500

    shoping cart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,8 +1,13 @@
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import '../scss/layout/shopping-cart.scss';
+
+import 'animate.css';
+
 const ShoppingCart = ()=>{
     const cart = useSelector((state) => state.cart)
+    let total = 0;
+
     console.log(cart)
     return (
         <div className="shopping-cart animate__animated animate__slideInRight">
@@ -10,7 +15,28 @@ const ShoppingCart = ()=>{
                 <span className="material-symbols-outlined bold">arrow_back_ios</span>
             </Link>
             <span className="bold primary">Shopping cart</span>
-            {cart && (<span className='muted'>no hay productos seleccionados</span>) }
+            <ul className='cart-product'>
+                {cart.map(element=>{
+                    if(element.quantity<=0) return;
+                    total += (element.quantity*element.price)
+                    return (
+                        <li key={element.id}>
+                            <span class="bold secondary bg-primary badge">{element.quantity}</span>
+                            <img 
+                                className=' animate__animated animate__fadeIn'
+                                src={`/assets/${element.src}`} 
+                                alt="img proof" 
+                            />
+                        </li>
+                    )
+                })}
+            </ul>
+            <ul className='totals'>
+                <li>
+                    <span className='muted bold'>Total</span>
+                    <span className='primary bold'>$ {total}</span>
+                </li>
+            </ul>
         </div>
     );
 }
--- src/pages/components/ShoppingIcon.jsx
+++ src/pages/components/ShoppingIcon.jsx
@@ -1,10 +1,15 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import '../../scss/layout/components/shopping-icon.scss'
 const Car = ()=>{
+    const cart = useSelector((state) => state.cart)
+    let cart_price = 0
+    cart.map(element => cart_price+=(element.quantity*element.price))
     return(
         <Link to={`shopping-cart`} >
             <div className="shopping-icon">
                 <span className="material-symbols-outlined">shopping_cart</span>
+                <span>$ {cart_price}</span>
             </div>
         </Link >
     );
